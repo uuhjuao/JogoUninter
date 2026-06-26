f2be5f2ce84fc1157c374b34d9baead5ed433775
Daniel Silva <seuemail@exemplo.com>
2026-06-25 22:07:36 -0300

    changes 25/06

--- code/entity.py
+++ code/entity.py
@@ -2,11 +2,17 @@
 # -*- coding: utf-8 -*-
 from abc import ABC
 
-# os inimigos e jogadores serão gerados daqui
-class Entity(ABC):
-    def __init__(self, name: str, position: tuple):
-        self.name = None
-        self.surf = None
+import pygame
 
+
+# os inimigos e jogadores serão gerados daqui
+class Entity(ABC): # uma classe abstrata implica quw todos os metodos serão implementados pelos seus fihos
+    def __init__(self, name: str, position: tuple):
+        self.name = name
+        self.surf = pygame.image.load('./asset/backgame.png')
+        self.rect = self.surf.get_rect(left=position[0], top=position[1])
+        self.speed = 0
+
+    @abstractmethod # isso aqui é um decorator
     def move(self, ):
-        pass
+
--- code/entityFactory.py
+++ code/entityFactory.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-class EntityFactory:
+class EntityFactory: #n utlizamos a entityFACTORY pra criar um objeto, ela instancia objetos que serão instanciados, por isso n se usa INIT
     def __init__(self):
         pass
 
-    def get_entity(self, entity_type):
+    def get_entity(entity_name: str, position : tuple(0,0)):
         pass
